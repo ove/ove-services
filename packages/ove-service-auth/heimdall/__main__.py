e85ec9ce2bbcc01600eefce47305d44eadfcb3ab
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2019-01-24 17:08:35 +0000

    Fix review comments

--- packages/ove-service-auth/heimdall/__main__.py
+++ packages/ove-service-auth/heimdall/__main__.py
@@ -1,3 +1,4 @@
+import logging
 from wsgiref import simple_server
 
 from heimdall import setup_app
@@ -6,7 +7,11 @@ from heimdall import setup_app
 # do not use this in production
 # this is a dev only method provided for convenience
 def main():
-    simple_server.make_server('0.0.0.0', 8080, setup_app()).serve_forever()  # nosec
+    app = setup_app()
+    logging.critical("Do not use this service in production")
+    logging.critical("This method is for development only")
+    logging.critical("Use the start.sh script to start the production server")
+    simple_server.make_server('0.0.0.0', 8080, app).serve_forever()  # nosec
 
 
 if __name__ == '__main__':
--- packages/ove-service-auth/heimdall/const.py
+++ packages/ove-service-auth/heimdall/const.py
@@ -2,13 +2,16 @@ FIELD_ID = "id"
 FIELD_PASSWORD = "password"
 FIELD_AUTH_TOKEN = "AUTH_TOKEN"
 FIELD_ACCESS_TOKENS = "ACCESS_TOKENS"
+
 # location of the auth token, used by the login api
 FIELD_TOKEN_TARGET = "target"
 TOKEN_TARGET_COOKIE = "cookie"
 TOKEN_TARGET_HEADER = "header"
+
 # these are standard JWT conventions used by PyJWT
 FIELD_EXPIRATION = "exp"
 FIELD_ISSUER = "iss"
+
 # lock constants
 FIELD_LOCK_TOKEN = "LOCK_TOKEN"
 FIELD_LOCK_STATUS = "status"
