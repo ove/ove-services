3444109b5a00b3c228d813696b6f4bf9b8705d47
Senaka Fernando <senaka@wso2.com>
2019-01-26 15:54:49 +0000

    Fixing ove/ove#82.

--- packages/ove-service-persistence-inmemory/src/server/api.js
+++ packages/ove-service-persistence-inmemory/src/server/api.js
@@ -1,12 +1,17 @@
 const HttpStatus = require('http-status-codes');
 
+// IMPORTANT: Do not introduce any logs for operations on a single key, except for error scenarios.
+// Logs have been left out from most of the methods on purpose, as it contributes to significant
+// log file sizes.
 module.exports = function (app, log, Utils) {
     let items = {};
-    // IMPORTANT: There are a number of checks for undefined, which is required since null, 0, false or '' could all be
-    // valid keys/values. None of the operations involving a single key provides any logs to avoid log-overflow. These
-    // methods do provide logs should anything fail.
     const getKeys = function (req, res) {
+        // Keys do not start with a slash like paths in a URI, and therefore we ignore it. If the
+        // path was '/', the key would be an empty string. For all other paths, the first path
+        // segment would be the name of app to which the key belongs to. The subsequent segments
+        // would correspond to the actual key used in an app.
         if (!req.url.substring(1)) {
+            // If the key was an empty string, we return a list for keys belonging to all apps.
             let result = {};
             Object.keys(items).forEach(function (appName) {
                 Object.keys(items[appName]).forEach(function (item) {
@@ -15,28 +20,33 @@ module.exports = function (app, log, Utils) {
             });
             log.debug('Getting list of all keys');
             Utils.sendMessage(res, HttpStatus.OK, JSON.stringify(result));
-        } else {
-            const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
-            if (key.indexOf('/') === -1) {
-                const appItems = items[key];
-                if (appItems === undefined) {
-                    Utils.sendEmptySuccess(res);
-                } else {
-                    let result = {};
-                    Object.keys(appItems).forEach(function (item) {
-                        result[item] = appItems[item].timestamp;
-                    });
-                    Utils.sendMessage(res, HttpStatus.OK, JSON.stringify(result));
-                }
+            return;
+        }
+
+        const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
+        if (key.indexOf('/') === -1) {
+            // Unlike in the set and delete methods, the get method accepts paths with a single
+            // segment. Such requests would return a list of keys belonging to a specific app.
+            const appItems = items[key];
+            if (appItems === undefined) {
+                Utils.sendEmptySuccess(res);
             } else {
-                const appName = key.substring(0, key.indexOf('/'));
-                const appKey = key.substring(key.indexOf('/') + 1);
-                if (items[appName] === undefined || items[appName][appKey] === undefined) {
-                    log.error('Key not found', 'app:', appName, 'key:', appKey);
-                    Utils.sendMessage(res, HttpStatus.NOT_FOUND, JSON.stringify({ error: 'key not found' }));
-                } else {
-                    Utils.sendMessage(res, HttpStatus.OK, JSON.stringify(items[appName][appKey]));
-                }
+                let result = {};
+                Object.keys(appItems).forEach(function (item) {
+                    result[item] = appItems[item].timestamp;
+                });
+                Utils.sendMessage(res, HttpStatus.OK, JSON.stringify(result));
+            }
+        } else {
+            const appName = key.substring(0, key.indexOf('/'));
+            const appKey = key.substring(key.indexOf('/') + 1);
+            if (items[appName] === undefined || items[appName][appKey] === undefined) {
+                log.error('Key not found', 'app:', appName, 'key:', appKey);
+                Utils.sendMessage(res, HttpStatus.NOT_FOUND,
+                    JSON.stringify({ error: 'key not found' }));
+            } else {
+                Utils.sendMessage(res, HttpStatus.OK,
+                    JSON.stringify(items[appName][appKey]));
             }
         }
     };
@@ -44,51 +54,64 @@ module.exports = function (app, log, Utils) {
     const deleteKey = function (req, res) {
         if (!req.url.substring(1)) {
             log.error('Key not provided');
-            Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'key not provided' }));
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'key not provided' }));
+            return;
+        }
+        const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
+        if (key.indexOf('/') === -1) {
+            log.error('Invalid key:', key);
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'invalid key' }));
+            return;
+        }
+
+        const appName = key.substring(0, key.indexOf('/'));
+        const appKey = key.substring(key.indexOf('/') + 1);
+        if (items[appName] === undefined) {
+            log.error('Invalid key:', appKey, ', app:', appName);
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'invalid key' }));
+        } else if (items[appName][appKey] === undefined) {
+            log.error('Key not found', 'app:', appName, 'key:', appKey);
+            Utils.sendMessage(res, HttpStatus.NOT_FOUND,
+                JSON.stringify({ error: 'key not found' }));
         } else {
-            const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
-            if (key.indexOf('/') === -1) {
-                log.error('Invalid', 'key:', key);
-                Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'invalid key' }));
-            } else {
-                const appName = key.substring(0, key.indexOf('/'));
-                const appKey = key.substring(key.indexOf('/') + 1);
-                if (items[appName] === undefined) {
-                    log.error('Invalid key,', 'app:', appName, 'key:', appKey);
-                    Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'invalid key' }));
-                } else if (items[appName][appKey] === undefined) {
-                    log.error('Key not found', 'app:', appName, 'key:', appKey);
-                    Utils.sendMessage(res, HttpStatus.NOT_FOUND, JSON.stringify({ error: 'key not found' }));
-                } else {
-                    delete items[appName][appKey];
-                    Utils.sendEmptySuccess(res);
-                }
-            }
+            delete items[appName][appKey];
+            Utils.sendEmptySuccess(res);
         }
     };
 
     const setKey = function (req, res) {
         if (!req.url.substring(1)) {
             log.error('Key not provided');
-            Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'key not provided' }));
-        } else if (req.body.value === undefined || req.body.type === undefined || req.body.timestamp === undefined) {
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'key not provided' }));
+            return;
+        } else if (req.body.value === undefined || req.body.type === undefined ||
+            req.body.timestamp === undefined) {
             log.error('Invalid item:', JSON.stringify(req.body));
-            Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'invalid item' }));
-        } else {
-            const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
-            if (key.indexOf('/') === -1) {
-                log.error('Invalid', 'key:', key);
-                Utils.sendMessage(res, HttpStatus.BAD_REQUEST, JSON.stringify({ error: 'invalid key' }));
-            } else {
-                const appName = key.substring(0, key.indexOf('/'));
-                const appKey = key.substring(key.indexOf('/') + 1);
-                if (items[appName] === undefined) {
-                    items[appName] = {};
-                }
-                items[appName][appKey] = JSON.parse(JSON.stringify(req.body));
-                Utils.sendEmptySuccess(res);
-            }
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'invalid item' }));
+            return;
         }
+
+        const key = req.url.substring(1).replace(/%2(F|f)/g, '/');
+        if (key.indexOf('/') === -1) {
+            log.error('Invalid key:', key);
+            Utils.sendMessage(res, HttpStatus.BAD_REQUEST,
+                JSON.stringify({ error: 'invalid key' }));
+            return;
+        }
+
+        const appName = key.substring(0, key.indexOf('/'));
+        const appKey = key.substring(key.indexOf('/') + 1);
+        if (items[appName] === undefined) {
+            // We are setting the very first key for an app.
+            items[appName] = {};
+        }
+        items[appName][appKey] = JSON.parse(JSON.stringify(req.body));
+        Utils.sendEmptySuccess(res);
     };
 
     app.get(/^(?=$|[/])/, getKeys);
